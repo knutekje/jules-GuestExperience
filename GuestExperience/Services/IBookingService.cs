282286884ed167bee5ad61fd55fc7500bb94d316
knutekje <knutekjersem@gmail.com>
2025-02-10 07:13:18 +0100

    yes

---
+++ GuestExperience/Services/IBookingService.cs
@@ -0,0 +1,12 @@
+using GuestExperience.Models;
+
+public interface IBookingService
+{
+    
+    public Task<Booking> CreateBookingAsync(Booking booking);
+    public Task<Booking> UpdateBookingAsync(Booking booking);
+    public Task DeleteBookingAsync(Booking booking);
+    public Task<IEnumerable<Booking>> GetAllBookings();
+    public Task<Booking> GetBookingById(int id);
+    
+}
